0af84f9c0862692cdec11d89637b48583c676693
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-19 17:00:34 -1000

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Arseniy Oksenoyt on 5/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
